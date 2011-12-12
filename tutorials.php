69a5cbf6d6acd534e05c6feba93e0e85795f9c6c
Shawn Wilkinson <super3boy@gmail.com>
2011-12-11 23:04:01 -0500

    code and tutorials

---
+++ tutorials.php
@@ -0,0 +1,52 @@
+<?php $page = 3; require_once('header.php'); ?>
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+	<h1>Tutorials</h1>
+</div>
+
+<!-- Row of columns -->
+<div class="row show-grid">
+	<div class="span8">
+	  <h2>Blender 2.5</h2>
+	  	  <!--<p>Free PHP video tutorials, scripts, and articles.</p>-->
+	      <ul class="media-grid">
+		  	<li><a href="#"><img class="thumbnail" src="http://placehold.it/450x200" alt="wildphp"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="#">View &raquo;</a>
+	      	<a class="btn" href="#">YouTube</a>
+          </p>
+	</div>
+	<div class="span8">
+	  <h2>Blender 2.4</h2>
+	      <!--<p>A large online community for <a href="http://blender.org">Blender</a>.</p>-->
+	      <ul class="media-grid">
+		  	<li><a href="#"><img class="thumbnail" src="http://placehold.it/450x200" alt="nystic"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="#">View &raquo;</a>
+	      	<a class="btn" href="#">YouTube</a>
+          </p>
+	</div>
+	<div class="span8">
+	  <h2>PHP</h2>
+	      <!--<p>A large online community for <a href="http://blender.org">Blender</a>.</p>-->
+	      <ul class="media-grid">
+		  	<li><a href="http://wildphp.com"><img class="thumbnail" src="http://placehold.it/450x200" alt="nystic"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="http://wildphp.com">Visit WildPHP &raquo;</a>
+          </p>
+	</div>
+	<div class="span8">
+	  <h2>Requests</h2>
+	      <!--<p>A large online community for <a href="http://blender.org">Blender</a>.</p>-->
+	      <ul class="media-grid">
+		  	<li><a href="#"><img class="thumbnail" src="http://placehold.it/450x200" alt="nystic"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="#">Contact &raquo;</a>
+          </p>
+	</div>	
+</div>
+<?php require_once('footer.php'); ?>
