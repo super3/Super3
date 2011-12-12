abe7cbef66ca18317bbb5842d1db2da74345dee9
Shawn Wilkinson <super3boy@gmail.com>
2011-12-12 02:20:35 -0500

    added friends.php

---
+++ friends.php
@@ -0,0 +1,30 @@
+<?php $page = 0; require_once('header.php'); ?>
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+	<h1>Friends</h1>
+</div>
+
+<!-- Row of columns -->
+<div class="row show-grid">
+	<div class="span8">
+	  <h2>AJ Moore</h2>
+	  	  <p>Created the awesome Super3 logo. Check out his comic!</p>
+	      <ul class="media-grid">
+		  	<li><a href="http://gudfit.com/"><img class="thumbnail" src="img/template/aj.png" alt="aj"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="http://gudfit.com/">Visit &raquo;</a>
+          </p>
+	</div>
+	<div class="span8">
+	  <h2>Dryicons</h2>
+	      <p>Social icons on the about page. Some really great icons you should check out.</p>
+	      <ul class="media-grid">
+		  	<li><a href="http://dryicons.com"><img class="thumbnail" src="img/template/icons.png" alt="icon"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="http://dryicons.com">Visit &raquo;</a>
+          </p>
+	</div>
+</div>
+<?php require_once('footer.php'); ?>
