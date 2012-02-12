30ac75d2be018bace2fb3430d8fdd53fd145653e
Shawn Wilkinson <super3boy@gmail.com>
2012-02-11 20:43:26 -0500

    revert - part 2

---
+++ code.php
@@ -0,0 +1,28 @@
+<?php $page = 4; require_once('header.php'); ?>
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+	<h1>Code</h1>
+</div>
+
+<!-- Row of columns -->
+<div class="row left-shift">
+	<div>
+	  <h2>Zombie Factory</h2>
+	  	  <p>A simple zombie shooting game made with <a href="http://pygame.org">PyGame</a> and Python.</p>
+	      <ul class="media-grid">
+		  	<li><a href="zombie-factory"><img class="thumbnail" src="img/template/zombie-factory.png" alt="zombiefactory"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="zombie-factory">More &raquo;</a>
+          </p>
+	  <h2>FastF12</h2>
+	      <p>A rendering tool for <a href="http://blender.org">Blender</a> made in C#.</p>
+	      <ul class="media-grid">
+		  	<li><a href="fastf12"><img class="thumbnail" src="img/template/fastf12.png" alt="fastf12"></a></li>
+		  </ul>
+	      <p>
+	      	<a class="btn" href="fastf12">More &raquo;</a>
+          </p>
+	</div>
+</div>
+<?php require_once('footer.php'); ?>
---
+++ fastf12.php
@@ -0,0 +1,59 @@
+<?php $page = 4; require_once('header.php'); ?>
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+	<h1>FastF12</h1>
+</div>
+
+<!-- Row of columns -->
+<div class="row left-shift">
+	<div>
+	  <h2>About</h2>
+	      <ul class="media-grid">
+		  	<li><a><img class="thumbnail" src="img/template/fastf12.png" alt="zombiefactory"></a></li>
+		  </ul>
+	      <p>
+	      	FastF12 is a rendering tool for <a href="http://blender.org">Blender</a>, a free open source 3D 
+	      	content creation suite, made in C#. It is currently in development, and about 80% complete.
+	      	FastF12 basically provides a GUI or Graphical User Interface for 
+	      	<a href="http://wiki.blender.org/index.php/Doc:2.5/Manual/Render/Command_Line">Blender's Command
+	        Line</a>, making it much easier to manage large or multiple render jobs. In the future this tool
+	        will be ported to either C++ or Python, so it will be available for all platforms. Feel free to
+	        poke around the the source code, or contribute. 
+      	 </p>
+	      <p>
+	      	<a class="btn" href="https://github.com/super3/FastF12/zipball/master">Download</a>
+	      	<a class="btn" href="https://github.com/super3/FastF12">Source Code (Github)</a>
+          </p>
+      <h2>Screenshots</h2>
+      	 <p>
+      	 	Click to expand...
+      	 	<div class="row">
+			 	<div class="span8"> 	
+				  <ul class="media-grid">
+				  	<li><a href="img/dev-log/screen1.png">
+				  		<img src="img/dev-log/shot1.png" alt="shot"></a></li>
+				  </ul>
+			 	</div>
+			 	<div class="span8"> 	
+				  <ul class="media-grid">
+				  	<li><a href="img/dev-log/screen2.png/">
+				  		<img src="img/dev-log/shot2.png" alt="shot"></a></li>
+				  </ul>
+			 	</div>
+			 	<div class="span8"> 	
+				  <ul class="media-grid">
+				  	<li><a href="img/dev-log/screen3.png">
+				  		<img src="img/dev-log/shot3.png" alt="shot"></a></li>
+				  </ul>
+			 	</div>
+			 	<div class="span8"> 	
+				  <ul class="media-grid">
+				  	<li><a href="img/dev-log/screen4.png">
+				  		<img src="img/dev-log/shot4.png" alt="shot"></a></li>
+				  </ul>
+			 	</div>
+		 	</div>
+      	 </p>
+	</div>
+</div>
+<?php require_once('footer.php'); ?>
