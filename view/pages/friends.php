2b499f1f31e5f831b833f81665242b05ff33dd6a
Shawn Wilkinson <super3boy@gmail.com>
2012-03-14 01:28:30 -0400

    strange commit + friends had wrong image url

--- view/pages/friends.php
+++ view/pages/friends.php
@@ -8,13 +8,13 @@
 	<div class="span6">
 	  <h2>AJ Moore</h2>
                 <p>Created the awesome Super3 logo. Check out his comic!</p>
-                <?php thumbnail("span6", "http://gudfit.com/", "img/template/aj.png"); ?>
+                <?php thumbnail("span6", "http://gudfit.com/", "view/images/template/aj.png"); ?>
 	      <p><a class="btn" href="http://gudfit.com/">Visit &raquo;</a></p>
 	</div>
 	<div class="span6">
 	  <h2>Dryicons</h2>
 	      <p>Social icons on the about page. Some really great icons you should check out.</p>
-              <?php thumbnail("span6", "http://dryicons.com", "img/template/icons.png"); ?>
+              <?php thumbnail("span6", "http://dryicons.com", "view/images/template/icons.png"); ?>
 	      <p><a class="btn" href="http://dryicons.com">Visit &raquo;</a></p>
 	</div>
 </div>
