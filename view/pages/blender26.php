c75ee7d02076e23349095c9708084e563ef4ccb8
Shawn Wilkinson <super3boy@gmail.com>
2012-03-23 18:03:16 -0400

    start of blender 2.6 page

---
+++ view/pages/blender26.php
@@ -0,0 +1,13 @@
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+    <h1>Blender 2.6</h1>
+</div>
+
+<!-- Row of columns -->
+<div class="row left-shift">
+    <?php thumbnail("span12", "#", "view/images/template/blender26-big.png"); ?>
+    <p>
+        <a class="btn" href="#">Play on YouTube</a>
+        <a class="btn" href="#">Goto YouTube Playlist</a>
+    </p>
+</div>
--- view/pages/tutorials.php
+++ view/pages/tutorials.php
@@ -1,28 +1,28 @@
 <!-- Main hero unit for a primary marketing message or call to action -->
 <div class="hero-unit">
-	<h1>Tutorials</h1>
+    <h1>Tutorials</h1>
 </div>
 
 <!-- Row of columns -->
 <div class="row show-grid">
-	<div class="span6">
-	  <h2>Blender 2.5</h2>
-           <?php thumbnail("span6", "blender25", "view/images/template/blender25.png"); ?>
-	   <p><a class="btn" href="blender25">View &raquo;</a></p>
-	</div>
-	<div class="span6">
-	  <h2>Blender 2.4</h2>
-           <?php thumbnail("span6", "blender24", "view/images/template/blender24.png"); ?>
-	   <p><a class="btn" href="blender24">View &raquo;</a></p>
-	</div>
-	<div class="span6">
-	  <h2>PHP</h2>
-           <?php thumbnail("span6", "http://wildphp.com", "view/images/template/php.png"); ?>
-	   <p><a class="btn" href="http://wildphp.com">Visit WildPHP &raquo;</a></p>
-	</div>
-	<div class="span6">
-	  <h2>Requests</h2>
-          <?php thumbnail("span6", "contact", "view/images/template/requests.png"); ?>
-	  <p><a class="btn" href="contact">Contact &raquo;</a></p>
-	</div>	
+    <div class="span6">
+        <h2>Blender 2.6</h2>
+        <?php thumbnail("span6", "blender26", "view/images/template/blender26.png"); ?>
+        <p><a class="btn" href="blender26">View &raquo;</a></p>
+    </div>
+    <div class="span6">
+        <h2>Blender 2.5</h2>
+        <?php thumbnail("span6", "blender25", "view/images/template/blender25.png"); ?>
+        <p><a class="btn" href="blender25">View &raquo;</a></p>
+    </div>
+    <div class="span6">
+        <h2>Blender 2.4</h2>
+        <?php thumbnail("span6", "blender24", "view/images/template/blender24.png"); ?>
+        <p><a class="btn" href="blender24">View &raquo;</a></p>
+    </div>
+    <div class="span6">
+        <h2>PHP</h2>
+        <?php thumbnail("span6", "http://wildphp.com", "view/images/template/php.png"); ?>
+        <p><a class="btn" href="http://wildphp.com">Visit WildPHP &raquo;</a></p>
+    </div>
 </div>
