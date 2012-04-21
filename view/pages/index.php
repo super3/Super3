394bbb49a0a3877869756a612d6d80337eb608fd
Shawn Wilkinson <super3boy@gmail.com>
2012-04-21 19:48:19 -0400

    -Added Sketch and Shake as the Hero Block -Moved Welcome Message Down

--- view/pages/index.php
+++ view/pages/index.php
@@ -1,33 +1,55 @@
 <!-- Main hero unit for a primary marketing message or call to action -->
 <div class="hero-unit">
-	<h1>Welcome!</h1>
-	<p>My name is Shawn Wilkinson, and  I'm an artist, web designer, developer, and teacher. On this website you can check of
-	some of the projects I've worked on, as well as help yourself to all the free tutorials, code snippets, models, references,
-	and textures I have to offer. Enjoy!</p>
-	<p><a href="about" class="btn btn-large btn-primary">Learn more &raquo;</a></p>
+	<div class="row">
+		<div class="span3">
+			<?php thumbnail("span3", "http://windowsphone.com/s?appid=5633a058-44da-4ddb-9132-42ce9ab52226", "view/images/template/sketchandshake.png"); ?>
+		</div>
+		<div class="span7">
+			<h1>Sketch and Shake</h1>
+			<p>
+				Check out my new and first Window 7 Phone App. Use your finger to draw on the screen, and
+				then when you want to erase just give the phone a shake.
+			</p>
+			<p><a href="http://windowsphone.com/s?appid=5633a058-44da-4ddb-9132-42ce9ab52226" class="btn btn-large btn-primary">
+				<i class="icon-download icon-white" style="margin-top: 2px;"></i> Download for Windows 7 Phone (Free!)</a></p>
+		</div>
+	</div>
 </div>
 
 <!-- Row of columns -->
 <div class="row">
-    <div class="span4">
-        <h2>Sites</h2>
-        <?php thumbnail("span4", "sites", "view/images/template/sites.png"); ?>
-        <p>I make tech websites for various topics, as well as freelance website development. Listed are some of the websites
-        I have made. Need a professional website done? You can contact me <a href="#">here</a>. </p>
-        <p><a class="btn" href="sites">View &raquo;</a></p>
-    </div>
-    <div class="span4">
-        <h2>Tutorials</h2>
-        <?php thumbnail("span4", "tutorials", "view/images/template/tutorials.png"); ?>
-        <p>I love to teach and talk about anything tech. All my free video and text tutorials are listed here just for you.
-        They include my popular <a href="http://blender.org">Blender</a> series, PHP, and many others. </p>
-        <p><a class="btn" href="tutorials">View &raquo;</a></p>
-    </div>
-    <div class="span4">
-        <h2>Code</h2>
-        <?php thumbnail("span4", "code", "view/images/template/code.png"); ?>
-        <p>See all the cool code projects I am currently working on. Small snippets and scripts are available for download. 
-        Everything else (my larger projects and collaborations) are available on <a href="https://github.com/super3">Github</a>.</p>
-        <p><a class="btn" href="code">View &raquo;</a></p>
+	<div>
+		<div class="span10">
+			<h2>Welcome</h2>
+			<p>My name is Shawn Wilkinson, and  I'm an artist, web designer, developer, and teacher. On this website you can check of
+			some of the projects I've worked on, as well as help yourself to all the free tutorials, code snippets, models, references,
+			and textures I have to offer.</p>
+		</div>
+		<div class="span2">
+			<p style="margin-top: 35px;"><a class="btn btn-large" href="sites">About Me &raquo;</a></p>
+		</div>
+	</div>
+	<div>
+	    <div class="span4">
+	        <h2>Sites</h2>
+	        <?php thumbnail("span4", "sites", "view/images/template/sites.png"); ?>
+	        <p>I make tech websites for various topics, as well as freelance website development. Listed are some of the websites
+	        I have made. Need a professional website done? You can contact me <a href="#">here</a>. </p>
+	        <p><a class="btn" href="sites">View &raquo;</a></p>
+	    </div>
+	    <div class="span4">
+	        <h2>Tutorials</h2>
+	        <?php thumbnail("span4", "tutorials", "view/images/template/tutorials.png"); ?>
+	        <p>I love to teach and talk about anything tech. All my free video and text tutorials are listed here just for you.
+	        They include my popular <a href="http://blender.org">Blender</a> series, PHP, and many others. </p>
+	        <p><a class="btn" href="tutorials">View &raquo;</a></p>
+	    </div>
+	    <div class="span4">
+	        <h2>Code</h2>
+	        <?php thumbnail("span4", "code", "view/images/template/code.png"); ?>
+	        <p>See all the cool code projects I am currently working on. Small snippets and scripts are available for download. 
+	        Everything else (my larger projects and collaborations) are available on <a href="https://github.com/super3">Github</a>.</p>
+	        <p><a class="btn" href="code">View &raquo;</a></p>
+	    </div>
     </div>
 </div>
